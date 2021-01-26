dacd6e4d19503df523920963fffc6ec33bc32049
Simen Ullern <simen.ullern@nav.no>
2021-01-26 11:51:35 +0100

    Patcher inn manglende dokumenter i basen
    Co-authored-by: Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
    Co-authored-by: Knut Nygaard <knut.nygaard@nav.no>

---
+++ src/main/resources/db/migration/V8__patcher_inn_manglende_dokumenter.sql
@@ -0,0 +1,2 @@
+INSERT INTO hendelse(dokument_id, hendelse_id, type) VALUES ('f199c324-cc90-424c-ad41-19ecca346e44', '25883c38-69e1-4620-94d7-85f1039fca9b', 'Sykmelding');
+INSERT INTO hendelse(dokument_id, hendelse_id, type) VALUES ('1dddcbba-e535-429f-8851-f96a8d6e46da', 'fba8dc55-d35f-440c-a6fe-8dc65852eb42', 'Søknad');
