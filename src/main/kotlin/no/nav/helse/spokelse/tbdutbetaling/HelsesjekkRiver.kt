727938025444e6b7c34b06a6428aae44b08ee91e
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2023-05-23 10:37:07 +0200

    Sender nå kun alert når noe er gæli

--- src/main/kotlin/no/nav/helse/spokelse/tbdutbetaling/HelsesjekkRiver.kt
+++ src/main/kotlin/no/nav/helse/spokelse/tbdutbetaling/HelsesjekkRiver.kt
@@ -3,7 +3,11 @@ package no.nav.helse.spokelse.tbdutbetaling
 import com.fasterxml.jackson.databind.JsonNode
 import no.nav.helse.rapids_rivers.*
 import org.slf4j.LoggerFactory
-import java.time.LocalDateTime
+import org.slf4j.event.Level
+import org.slf4j.event.Level.ERROR
+import org.slf4j.event.Level.INFO
+import java.time.DayOfWeek
+import java.time.DayOfWeek.THURSDAY
 import java.time.temporal.ChronoUnit.HOURS
 import java.time.temporal.ChronoUnit.WEEKS
 
@@ -15,21 +19,22 @@ internal class HelsesjekkRiver(
     init {
         River(rapidsConnection).apply {
             validate {
-                it.demandValue("@event_name", "hel_time")
-                it.requireKey("system_participating_services")
+                it.demandValue("@event_name", "halv_time")
+                it.requireKey("system_participating_services", "@opprettet")
                 it.rejectValues("ukedag", listOf("SATURDAY", "SUNDAY"))
             }
         }.register(this)
     }
 
     override fun onPacket(packet: JsonMessage, context: MessageContext) {
-        if (ignorer) return
+        if (!packet.utførHelsesjekk) return
 
         val systemParticipatingServices = packet["system_participating_services"]
+        val ukedag = DayOfWeek.valueOf(packet["ukedag"].asText())
+
         try {
-            val slackAlarm = Helsesjekk(dao, systemParticipatingServices).slackAlarm() ?: return sikkerlogg.info("Spøkelse fungerer som den skal")
-            sikkerlogg.error("Kan se ut til at Spøkelse har problemer, sender alarm på slack:\n\t${slackAlarm}")
-            context.publish(slackAlarm)
+            val slackmelding = Helsesjekk(dao, systemParticipatingServices, ukedag).slackmelding() ?: return
+            context.publish(slackmelding)
         } catch (throwable: Throwable) {
             sikkerlogg.error("Feil ved utføring av Spøkelse sin helsesjekk", throwable)
         }
@@ -37,20 +42,18 @@ internal class HelsesjekkRiver(
 
     private companion object {
         private val sikkerlogg = LoggerFactory.getLogger("tjenestekall")
-        private val ignorer get() = LocalDateTime.now().hour != 9
+        private val JsonMessage.utførHelsesjekk get() = get("@opprettet").asLocalDateTime().let { it.hour == 8 && it.minute == 30 }
 
-        class Helsesjekk(dao: TbdUtbetalingDao, private val systemParticipatingServices: JsonNode) {
+        class Helsesjekk(dao: TbdUtbetalingDao, private val systemParticipatingServices: JsonNode, ukedag: DayOfWeek) {
             private val arbeidsgiverutbetalinger = dao.arbeidsgiverutbetalinger(arbeidsgiverutbetalingerTidsrom)
             private val arbeidsgiverannulleringer = dao.arbeidsgiverannulleringer(arbeidsgiverAnnulleringerTidsrom)
             private val personutbetalinger = dao.personutbetalinger(personutbetalingerTidsrom)
             private val personannulleringer = dao.personannulleringer(personannulleringerTidsrom)
 
             private val farePåFerde = listOf(arbeidsgiverutbetalinger, personutbetalinger, arbeidsgiverannulleringer, personannulleringer).any { it == 0 }
+            private val gladmelding = ukedag == THURSDAY
 
-            private val melding get() = """
-Hen husker så godt Spøkelse-gate, og ønsker nødig at jeg skal havne i samme situasjon igjen!
-Så jeg tok en titt på utbetalingene jeg har registret i det siste, ser ikke dette litt lite ut? :pinching_hand:
-
+            private val oppsummering get() = """
 :briefcase: Arbeidsgiver :briefcase:
 Utbetalinger siste ${arbeidsgiverutbetalingerTidsrom.first} ${arbeidsgiverutbetalingerTidsrom.second.name}: $arbeidsgiverutbetalinger
 Annulleringer siste ${arbeidsgiverAnnulleringerTidsrom.first} ${arbeidsgiverAnnulleringerTidsrom.second.name}: $arbeidsgiverannulleringer
@@ -62,13 +65,33 @@ Annulleringer siste ${personannulleringerTidsrom.first} ${personannulleringerTid
 - Deres erbødig SPøkelse :ghostie:
 """
 
-            internal fun slackAlarm(): String? {
-                //if (!farePåFerde) return null
-                return JsonMessage.newMessage("slackmelding", mapOf(
-                    "melding" to melding,
-                    "level" to "ERROR",
-                    "system_participating_services" to systemParticipatingServices
-                )).toJson()
+            private val varsel get() = """
+
+Hen husker så godt Spøkelse-gate, og ønsker nødig at jeg skal havne i samme situasjon igjen!
+Så jeg tok en titt på utbetalingene jeg har registret i det siste, ser ikke dette litt lite ut? :pinching_hand:
+
+$oppsummering
+"""
+
+            private val melding get() = """
+
+Bare en oppsummering fra meg denne gangen, jeg ser ikke noe bekymringsverdig med disse tallen. Gjør du? :index_pointing_at_the_viewer:
+
+$oppsummering
+"""
+
+            internal fun slackmelding(): String? {
+                if (farePåFerde) return lagSlackmelding(ERROR, varsel)
+                if (gladmelding) return lagSlackmelding(INFO, melding)
+                return null
+            }
+
+            private fun lagSlackmelding(level: Level, melding: String) = JsonMessage.newMessage("slackmelding", mapOf(
+                "melding" to melding,
+                "level" to level.name,
+                "system_participating_services" to systemParticipatingServices
+            )).toJson().also {
+                if (level == ERROR) sikkerlogg.error("Kan se ut til at Spøkelse har problemer, sender alarm på slack:\n\t${it}")
             }
 
             private companion object {
