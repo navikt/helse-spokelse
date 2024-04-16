05d616f1ef283446864ad47c27ff288cd44a6a49
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2024-04-16 08:50:18 +0200

    Logger tidsbruk på oppslag på gamle utbetalinger 🪵

--- src/main/kotlin/no/nav/helse/spokelse/gamlevedtak/HentVedtakDao.kt
+++ src/main/kotlin/no/nav/helse/spokelse/gamlevedtak/HentVedtakDao.kt
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory
 import java.time.LocalDate
 import java.time.LocalDateTime
 import javax.sql.DataSource
+import kotlin.time.measureTimedValue
 
 internal class HentVedtakDao(private val dataSource: () -> DataSource): TbdUtbetalingObserver {
 
@@ -100,74 +101,83 @@ internal class HentVedtakDao(private val dataSource: () -> DataSource): TbdUtbet
         }
     }
 
-    fun hentSpøkelsePerioder(fødselsnummer: String, fom: LocalDate, tom: LocalDate): Set<SpøkelsePeriode> {
+    internal fun hentSpøkelsePerioder(fødselsnummer: String, fom: LocalDate, tom: LocalDate): Set<SpøkelsePeriode> {
         if (!harData(fom)) return emptySet()
-        @Language("PostgreSQL")
-        val vedtakOppdragOgUtbetalingQuery = """
-            SELECT o.fagsystemid fagsystem_id,
-                u.fom         fom,
-                u.tom         tom,
-                u.grad        grad,
-                v.opprettet   utbetalt_tidspunkt,
-                v.orgnummer   organisasjonsnummer,
-                'VedtakOppdragOgUtbetaling' tag
-            FROM vedtak v
-            INNER JOIN oppdrag o on v.id = o.vedtak_id
-            INNER JOIN utbetaling u on o.id = u.oppdrag_id
-            WHERE v.fodselsnummer = :fodselsnummer
-            AND u.tom >= :fom AND NOT u.fom > :tom
-        """
+        return hentFraDb(fødselsnummer, fom, tom)
+    }
 
-        @Language("PostgreSQL")
-        val oldVedtakOgOldUtbetalingQuery = """
-            SELECT
-                (SELECT distinct vu.utbetalingsref FROM vedtak_utbetalingsref vu WHERE vu.vedtaksperiode_id = ov.vedtaksperiode_id) fagsystem_id,
-                ou.fom fom,
-                ou.tom tom,
-                ou.grad grad,
-                ov.opprettet utbetalt_tidspunkt,
-                ov.orgnummer organisasjonsnummer,
-                'OldVedtakOgOldUtbetaling' tag
-            FROM old_vedtak ov
-            INNER JOIN old_utbetaling ou on ov.id = ou.vedtak_id
-            WHERE ov.fodselsnummer = :fodselsnummer
-            AND ou.tom >= :fom AND NOT ou.fom > :tom
-        """
+    private fun hentFraDb(fødselsnummer: String, fom: LocalDate, tom: LocalDate): Set<SpøkelsePeriode> {
+        val (perioder, duration) = measureTimedValue {
+            @Language("PostgreSQL")
+            val vedtakOppdragOgUtbetalingQuery = """
+                SELECT o.fagsystemid fagsystem_id,
+                    u.fom         fom,
+                    u.tom         tom,
+                    u.grad        grad,
+                    v.opprettet   utbetalt_tidspunkt,
+                    v.orgnummer   organisasjonsnummer,
+                    'VedtakOppdragOgUtbetaling' tag
+                FROM vedtak v
+                INNER JOIN oppdrag o on v.id = o.vedtak_id
+                INNER JOIN utbetaling u on o.id = u.oppdrag_id
+                WHERE v.fodselsnummer = :fodselsnummer
+                AND u.tom >= :fom AND NOT u.fom > :tom
+            """
 
-        @Language("PostgreSQL")
-        val gamleUtbetalingerQuery = """
-            SELECT
-                fagsystem_id,
-                fom,
-                tom,
-                grad,
-                opprettet utbetalt_tidspunkt,
-                orgnummer organisasjonsnummer,
-                'GamleUtbetalinger' tag
-            FROM gamle_utbetalinger
-            WHERE fodselsnummer = :fodselsnummer
-            AND tom >= :fom AND NOT fom > :tom
-        """
+            @Language("PostgreSQL")
+            val oldVedtakOgOldUtbetalingQuery = """
+                SELECT
+                    (SELECT distinct vu.utbetalingsref FROM vedtak_utbetalingsref vu WHERE vu.vedtaksperiode_id = ov.vedtaksperiode_id) fagsystem_id,
+                    ou.fom fom,
+                    ou.tom tom,
+                    ou.grad grad,
+                    ov.opprettet utbetalt_tidspunkt,
+                    ov.orgnummer organisasjonsnummer,
+                    'OldVedtakOgOldUtbetaling' tag
+                FROM old_vedtak ov
+                INNER JOIN old_utbetaling ou on ov.id = ou.vedtak_id
+                WHERE ov.fodselsnummer = :fodselsnummer
+                AND ou.tom >= :fom AND NOT ou.fom > :tom
+            """
 
-        val sammenstiltQuery = """
-            $vedtakOppdragOgUtbetalingQuery UNION ALL $oldVedtakOgOldUtbetalingQuery UNION ALL $gamleUtbetalingerQuery
-        """
+            @Language("PostgreSQL")
+            val gamleUtbetalingerQuery = """
+                SELECT
+                    fagsystem_id,
+                    fom,
+                    tom,
+                    grad,
+                    opprettet utbetalt_tidspunkt,
+                    orgnummer organisasjonsnummer,
+                    'GamleUtbetalinger' tag
+                FROM gamle_utbetalinger
+                WHERE fodselsnummer = :fodselsnummer
+                AND tom >= :fom AND NOT fom > :tom
+            """
 
-        val annullerteFagsystemIder = hentAnnuleringerForFødselsnummer(fødselsnummer)
+            @Language("PostgreSQL")
+            val sammenstiltQuery = """
+                $vedtakOppdragOgUtbetalingQuery UNION ALL $oldVedtakOgOldUtbetalingQuery UNION ALL $gamleUtbetalingerQuery
+            """
 
-        return sessionOf(dataSource()).use { session ->
-            session.run(queryOf(sammenstiltQuery, mapOf("fodselsnummer" to fødselsnummer, "fom" to fom, "tom" to tom)).map { row ->
-                if (row.string("fagsystem_id") in annullerteFagsystemIder) null
-                else SpøkelsePeriode(
-                    personidentifikator = Personidentifikator(fødselsnummer),
-                    fom = row.localDate("fom"),
-                    tom = row.localDate("tom"),
-                    grad = row.int("grad"),
-                    organisasjonsnummer = row.string("organisasjonsnummer"),
-                    tags = setOf("Spleis", row.string("tag"))
-                )
-            }.asList).toSet()
+            val annullerteFagsystemIder = hentAnnuleringerForFødselsnummer(fødselsnummer)
+
+            sessionOf(dataSource()).use { session ->
+                session.run(queryOf(sammenstiltQuery, mapOf("fodselsnummer" to fødselsnummer, "fom" to fom, "tom" to tom)).map { row ->
+                    if (row.string("fagsystem_id") in annullerteFagsystemIder) null
+                    else SpøkelsePeriode(
+                        personidentifikator = Personidentifikator(fødselsnummer),
+                        fom = row.localDate("fom"),
+                        tom = row.localDate("tom"),
+                        grad = row.int("grad"),
+                        organisasjonsnummer = row.string("organisasjonsnummer"),
+                        tags = setOf("Spleis", row.string("tag"))
+                    )
+                }.asList).toSet()
+            }
         }
+        sikkerLogg.info("Oppslag mot gamle utbetalinger tok ${duration.inWholeMilliseconds}ms")
+        return perioder
     }
 
     override fun utbetaling(meldingId: Long, utbetaling: Utbetaling) {
