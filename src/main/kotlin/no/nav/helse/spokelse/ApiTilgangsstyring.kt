2594c5d02bdfbd180fd123f0e253175f82cc7ca5
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2024-05-24 15:54:15 +0200

    Legger til Allowlist også for /grunnlag
    Det blir uansett håndhevet ved at det kun er appene definert i NAIS-yml at det kun er disse som kan requeste.
    
    Men får litt mer oversiktelig logging om hvem som sender requester

--- src/main/kotlin/no/nav/helse/spokelse/ApiTilgangsstyring.kt
+++ src/main/kotlin/no/nav/helse/spokelse/ApiTilgangsstyring.kt
@@ -15,12 +15,23 @@ internal object ApplicationIdAllowlist: ApiTilgangsstyring {
         call.håndhevTilgangTil("utbetalte-perioder", AllowlistUtbetaltePerioder)
 
     override fun grunnlag(call: ApplicationCall) {
-        sikkerlogg.info("Håndterer request til /grunnlag fra Foreldrepenger/K9 (${call.applicationId})")
+        call.håndhevTilgangTil("grunnlag", AllowlistGrunnlag)
     }
 
     private val AllowlistUtbetaltePerioder = mapOf(
         "885a87c7-d4c4-4ace-8b1c-a8da50eb719c" to "spapi-dev",
         "11c3bc3d-1da7-4598-a8c4-73bead228a90" to "spapi-prod"
+    )
+
+    private val AllowlistGrunnlag = mapOf(
+        "7533b639-ec49-4531-80fd-752f412c3b5a" to "k9-abakus-prod",
+        "0fc8e157-984d-46f8-911a-52f2b229fef9" to "fpabakus-prod",
+        "17f6b413-aba3-46be-bdde-793ad7081e28" to "fpsak-prod",
+        "6b779173-1246-41db-9f04-da4294a39a6c" to "fprisk-prod",
+        "650e72f0-19a1-40ea-849f-8a3e986dbce9" to "k9-abakus-dev",
+        "cb082ea5-4eec-4a1e-8bab-cf04181adaeb" to "fpabakus-dev",
+        "11815622-aad9-49b5-997e-5d3c5b9c12f7" to "fpsak-dev",
+        "dbaac815-e57c-41bf-9674-125b76e567c8" to "fprisk-dev",
     )
 
     private val sikkerlogg = LoggerFactory.getLogger("tjenestekall")
