51169fd04e1991919d80093702a100cf56fd0a4b
david steinsland <david@davidsteinsland.net>
2024-11-18 09:40:25 +0100

    bumper r&r

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-val rapidsAndRiversVersion = "2024111509181731658731.11009b44c672"
+val rapidsAndRiversVersion = "2024111809201731918023.7c8474fdd5eb"
 val tbdLibsVersion = "2024.11.15-09.09-08ca346b"
 val ktorVersion = "3.0.1"
 val postgresqlVersion = "42.7.4"
