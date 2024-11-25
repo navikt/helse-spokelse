535ecda6840c74299f0e28efd409b1dd2d947ee8
david steinsland <david@davidsteinsland.net>
2024-11-25 11:06:40 +0100

    bumper tbd-libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 val rapidsAndRiversVersion = "2024112510241732526640.8542991368ca"
-val tbdLibsVersion = "2024.11.25-10.23-348b187b"
+val tbdLibsVersion = "2024.11.25-10.59-6f263a10"
 val ktorVersion = "3.0.1"
 val postgresqlVersion = "42.7.4"
 val wiremockVersion = "3.9.2"
