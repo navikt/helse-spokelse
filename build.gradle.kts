2489a877ecac8786e16c4f00320fd73d939d279b
david steinsland <david@davidsteinsland.net>
2025-03-30 13:23:17 +0200

    bumper r&r

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
-val rapidsAndRiversVersion = "2025032719521743101565.4abbf0c3af65"
-val tbdLibsVersion = "2025.03.27-18.30-c228796d"
+val rapidsAndRiversVersion = "2025033013081743332933.20de2f3d8983"
+val tbdLibsVersion = "2025.03.30-13.02-f7cb11ef"
 val ktorVersion = "3.1.2"
 val postgresqlVersion = "42.7.5"
 val wiremockVersion = "3.9.2"
