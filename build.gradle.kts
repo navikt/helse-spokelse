34f52bc8a8a18303e7f467b8ec5a1333124f3125
Hakon Arneng Holmstedt <hakon.a.holmstedt@gmail.com>
2026-01-22 09:56:03 +0100

    Bumper tbd-libs, for å kunne få nytt docker-image, for å lukke noen CVEer

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 val rapidsAndRiversVersion = "2026011411051768385145.e8ebad1177b4"
-val tbdLibsVersion = "2025.11.04-10.12-3a167132"
+val tbdLibsVersion = "2026.01.22-09.16-1d3f6039"
 val ktorVersion = "3.2.3"
 val postgresqlVersion = "42.7.7"
 val junitJupiterVersion = "5.12.1"
