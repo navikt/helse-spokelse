f165da4d699d0fe8ba2d5629d647d37200d7ac82
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2026-02-26 14:12:41 +0100

    Bump Kotlin JVM -> 2.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ val awaitilityVersion = "4.2.0"
 val mainClass = "no.nav.helse.spokelse.AppKt"
 
 plugins {
-    kotlin("jvm") version "2.2.21"
+    kotlin("jvm") version "2.3.0"
 }
 
 dependencies {
